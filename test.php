a8d92c725eeee035e1a2d931a9644646a23c0f61
trannois <harold.trannois@u-picardie.fr>
2022-03-15 11:32:40 +0100

    premier test travis avec phpcs

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+
+/**
+ * @file
+ * Description of what this module (or file) is doing.
+ */
+
+/**
+ * Implements hook_help().
+ */
+function yourmodule_help($path, $arg)
+{
+    // or any other hook...
+}
